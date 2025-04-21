785711eefea005a63f02fbe8ef47d5e4b11150b5
shn <sreeharinagesh@gmail.com>
2025-04-21 10:03:59 +0000

    Init phaser.

---
+++ game.js
@@ -0,0 +1,11 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {}
+function create() {}
+function update() {}
