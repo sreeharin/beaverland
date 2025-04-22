fbc0658ebf4ff3ef6a5cea43fbd2ad60e641dddf
shn <sreeharinagesh@gmail.com>
2025-04-22 10:23:06 +0000

    added avatar to game

--- game.js
+++ game.js
@@ -2,10 +2,30 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: '#87CEEB',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
 var game = new Phaser.Game(config);
 
-function preload() {}
-function create() {}
-function update() {}
+var player;
+
+function preload() {
+    this.load.image('player', 'assets/beaver.png');
+}
+
+function create() {
+    player = this.add.sprite(400, 300, 'player').setScale(.10);
+
+    this.input.on('pointerdown', function (pointer) {
+        player.x = pointer.x;
+        player.y = pointer.y;
+    });
+}
+
+function update() {
+}
